3d36f5635ff831cc0773e64e71a883acb408c04d
FonsecaJoao <joaomgfonseca@gmail.com>
2021-05-18 00:28:31 +0100

    Remove loading screen and logic from app root

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,8 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Observable, Subscription } from 'rxjs';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit, OnDestroy {
-  private readonly subscriptions: Subscription[] = [];
-  public statusOkay: boolean = false;
-
-  constructor(private httpClient: HttpClient) { }
-
-  checkServerHealth() {
-    this.subscriptions.push(this.httpClient.get<any>('https://private-bbbe9-blissrecruitmentapi.apiary-mock.com/health').subscribe(data => {
-      this.statusOkay = data.status !== 'OK' ? true : false;
-    }));
-
-  }
-
-  ngOnInit(): void {
-    this.checkServerHealth();
-  }
-
-  ngOnDestroy(): void {
-    this.subscriptions.forEach(sub => {
-      sub.unsubscribe();
-    });
-  }
-}
+export class AppComponent {}
