234c8694048e8b687f16b3b9c3b8c913019bb716
FonsecaJoao <joaomgfonseca@gmail.com>
2021-05-18 00:30:14 +0100

    Add code and logic from app root to loading screen

---
+++ src/app/loading-screen/loading-screen.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { LoadingScreenService } from './service/loading-screen.service';
+
+@Component({
+  selector: 'app-loading-screen',
+  templateUrl: './loading-screen.component.html',
+  styleUrls: ['./loading-screen.component.scss']
+})
+
+export class LoadingScreenComponent implements OnInit, OnDestroy {
+  private readonly subscriptions: Subscription[] = [];
+  public statusOkay: boolean = false;
+  public isLoading: boolean = true;
+
+  constructor(private loadingService: LoadingScreenService,
+    private router: Router) { }
+
+  ngOnInit(): void {
+    this.subscribeRequests();
+    this.checkServerHealth();
+  }
+
+  subscribeRequests() {
+    this.subscriptions.push(this.loadingService.serverStatus$.subscribe(data => {
+      this.isLoading = false;
+      if (data.status !== 'OK') {
+        this.statusOkay = true;
+        this.router.navigate(['/questions'])
+      } else {
+        this.statusOkay = false;
+      }
+    }));
+  }
+
+  checkServerHealth() {
+    this.isLoading = true;
+    this.loadingService.checkServerHealth();
+  }
+
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(sub => {
+      sub.unsubscribe();
+    });
+  }
+}
