e26643d052ecd32fe0da07c2e9debabd70a64f5b
FonsecaJoao <joaomgfonseca@gmail.com>
2021-05-17 23:44:57 +0100

    Remove generated code and start implementation of loading screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'r-app';
+export class AppComponent implements OnInit, OnDestroy {
+  private readonly subscriptions: Subscription[] = [];
+  public statusOkay: boolean = false;
+
+  constructor(private httpClient: HttpClient) { }
+
+  checkServerHealth() {
+    this.subscriptions.push(this.httpClient.get<any>('https://private-bbbe9-blissrecruitmentapi.apiary-mock.com/health').subscribe(data => {
+      this.statusOkay = data.status !== 'OK' ? true : false;
+    }));
+
+  }
+
+  ngOnInit(): void {
+    this.checkServerHealth();
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(sub => {
+      sub.unsubscribe();
+    });
+  }
 }
