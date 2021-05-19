b54fb7a1f927f125a76cd710a361df44f27825d3
FonsecaJoao <joaomgfonseca@gmail.com>
2021-05-19 01:31:34 +0100

    Add questions component to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,11 +10,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoadingScreenComponent } from './loading-screen/loading-screen.component';
+import { QuestionsListComponent } from './questions/list/questions-list/questions-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoadingScreenComponent
+    LoadingScreenComponent,
+    QuestionsListComponent
   ],
   imports: [
     BrowserModule,
