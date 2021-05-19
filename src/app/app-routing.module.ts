043c4256ceb56740e509affc18f57c749a46ddcf
FonsecaJoao <joaomgfonseca@gmail.com>
2021-05-19 01:30:27 +0100

    Add routing for questions component and loading screen

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoadingScreenComponent } from './loading-screen/loading-screen.component';
+import { QuestionsListComponent } from './questions/list/questions-list/questions-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoadingScreenComponent },
+  { path: 'questions', component: QuestionsListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
