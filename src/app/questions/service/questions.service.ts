c806a2fe072145a1adeb7ff7a1cc464bbdf91c01
FonsecaJoao <joaomgfonseca@gmail.com>
2021-05-20 02:26:23 +0100

    Add method to get questions list

--- src/app/questions/service/questions.service.ts
+++ src/app/questions/service/questions.service.ts
@@ -1,9 +1,30 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+
+export interface QuestionsList {
+  choices: { choice: string, votes: number }[];
+  id: number;
+  image_url: string;
+  published_at: string;
+  question: string;
+  thumb_url: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class QuestionsService {
+  public questionsList$: Subject<QuestionsList[]> = new Subject<QuestionsList[]>()
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public getQuestions(limit: number, offset: number, filter: string) {
+    return this.http.get<QuestionsList[]>(
+      `https://private-bbbe9-blissrecruitmentapi.apiary-mock.com/questions?limit=${limit}&offset=${offset}&filter=${filter}`
+    ).subscribe(data => {
+
+      this.questionsList$.next(data);
+    });
+  }
 }
