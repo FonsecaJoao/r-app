16e42d79226663f6db446e0d7dd3116f61cbd161
FonsecaJoao <joaomgfonseca@gmail.com>
2021-05-20 02:51:34 +0100

    Add table to questions list

--- src/app/questions/list/questions-list/questions-list.component.ts
+++ src/app/questions/list/questions-list/questions-list.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { QuestionsList, QuestionsService } from '../../service/questions.service';
 
 @Component({
   selector: 'app-questions-list',
   templateUrl: './questions-list.component.html',
   styleUrls: ['./questions-list.component.scss']
 })
-export class QuestionsListComponent implements OnInit {
+export class QuestionsListComponent implements OnInit, AfterViewInit {
+  readonly subscriptions: Subscription[] = [];
+  displayedColumns: string[] = ['id', 'question', 'image_url', 'thumb_url', 'published_at'];
+  dataSource: MatTableDataSource<QuestionsList> = new MatTableDataSource<QuestionsList>();
 
-  constructor() { }
+
+
+  constructor(private questionsService: QuestionsService,
+    private router: Router) { }
 
   ngOnInit(): void {
+    this.subscribeRequests();
+    this.questionsService.getQuestions(10, 0, '')
+  }
+
+  subscribeRequests() {
+    this.subscriptions.push(this.questionsService.questionsList$.subscribe(data => {
+      this.dataSource.data = data;
+    }));
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.questionsService.getQuestions(10, 0, filterValue.trim().toLowerCase());
+  }
+
+  changePage(event: any) {
+    console.log('TODO: on page change request questionsList' , event);
   }
 
 }
