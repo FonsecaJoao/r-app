44ba7373bdb22f4f38c5803320dbc63ca7fae14f
FonsecaJoao <joaomgfonseca@gmail.com>
2021-05-22 02:25:10 +0100

    Implement monitor connection service in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,25 @@
 import { Component } from '@angular/core';
+import { ConnectionService } from 'ngx-connection-service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent {
+  public hasNetworkConnection!: boolean;
+  public hasInternetAccess!: boolean;
+  public status!: string;
+ 
+  constructor(private connectionService: ConnectionService) {
+    this.connectionService.monitor().subscribe(currentState => {
+      this.hasNetworkConnection = currentState.hasNetworkConnection;
+      this.hasInternetAccess = currentState.hasInternetAccess;
+      if (this.hasNetworkConnection && this.hasInternetAccess) {
+        this.status = 'ONLINE';
+      } else {
+        this.status = 'OFFLINE';
+      }
+    });
+  }
+}
